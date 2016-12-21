b58801d3e3953d7a0069cf2ad009fee340799bf5
Manny Litos <emmanuel.litos@gmail.com>
2016-12-21 14:15:14 -0500

    Add files via upload

---
+++ form_2.php
@@ -0,0 +1,52 @@
+<HTML>
+<HEAD>
+</HEAD>
+<BODY>
+
+<form action="form_script_2.php" method="POST">
+
+<p>Name: <input type="text" name="name" size="30"/></p>
+
+<p>Team: <select name="team">
+<option value="Buffalo Bills">Buffalo Bills</option>
+<option value="Miami Dolphins">Miami Dolphins</option>
+<option value="New England Patriots">New England Patriots</option>
+<option value="New York Jets">New York Jets</option>
+<option value="Baltimore Ravens">Baltimore Ravens</option>
+<option value="Cincinnati Bengals">Cincinnati Bengals</option>
+<option value="Cleveland Browns">Cleveland Browns</option>
+<option value="Pittsburgh Steelers">Pittsburgh Steelers</option>
+<option value="Houston Texans">Houston Texans</option>
+<option value="Indianapolis Colts">Indianapolis Colts</option>
+<option value="Jacksonville Jaguars">Jacksonville Jaguars</option>
+<option value="Tennessee Titans">Tennessee Titans</option>
+<option value="Denver Broncos">Denver Broncos</option>
+<option value="Kansas City Chiefs">Kansas City Chiefs</option>
+<option value="Oakland Raiders">Oakland Raiders</option>
+<option value="San Diego Chargers">San Diego Chargers</option>
+<option value="Dallas Cowboys">Dallas Cowboys</option>
+<option value="New York Giants">New York Giants</option>
+<option value="Philadelphia Eagles">Philadelphia Eagles</option>
+<option value="Washington Redskins">Washington Redskins</option>
+<option value="Chicago Bears">Chicago Bears</option>
+<option value="Detroit Lions">Detroit Lions</option>
+<option value="Green Bay Packers">Green Bay Packers</option>
+<option value="Minnesota Vikings">Minnesota Vikings</option>
+<option value="Atlanta Falcons">Atlanta Falcons</option>
+<option value="Carolina Panthers">Carolina Panthers</option>
+<option value="New Orleans Saints">New Orleans Saints</option>
+<option value="Tampa Bay Buccaneers">Tampa Bay Buccaneers</option>
+<option value="Arizona Cardinals">Arizona Cardinals</option>
+<option value="Los Angeles Rams">Los Angeles Rams</option>
+<option value="San Francisco 49ers">San Francisco 49ers</option>
+<option value="Seattle Seahawks">Seattle Seahawks</option>
+</select></p>
+
+<p>Gender: <input type="radio" name="gender" value="Female"/> Female
+<input type="radio" name="gender" value="Male"/>Male</p>
+
+<input type="submit" name="submit" value="Submit"/>
+
+
+</BODY>
+</HTML>
---
+++ form_script_2.php
@@ -0,0 +1,10 @@
+<?php
+
+$name=$_POST['name'];
+$team=$_POST['team'];
+$gender=$_POST['gender'];
+
+print "<p>Name: $name</br>Team: $team</br>Gender: $gender</p>";
+
+
+?>
