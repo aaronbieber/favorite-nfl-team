250ca66dc3cb3823b8a0781877e6ba732a7068d1
Aaron Bieber <aaron@aaronbieber.com>
2016-12-21 16:48:37 -0500

    Refactor the shit out of everything

--- index.php
+++ index.php
@@ -1,52 +1,42 @@
-<HTML>
-<HEAD>
-</HEAD>
-<BODY>
+<?php
+require_once "header.php";
+require_once "teams.php";
+?>
+<form action="process_form.php" method="POST">
 
-<form action="form_script_2.php" method="POST">
+  <p>Name: <input type="text" name="name" size="30"/></p>
 
-<p>Name: <input type="text" name="name" size="30"/></p>
+  <p>Team: <select name="team">
+    <?php
+    foreach($teams as $id => $team_name) {
+      ?>
+      <option value="<?= $id ?>"><?= $team_name ?></option>
+      <?php
+    }
+    ?>
+  </select></p>
 
-<p>Team: <select name="team">
-<option value="Buffalo Bills">Buffalo Bills</option>
-<option value="Miami Dolphins">Miami Dolphins</option>
-<option value="New England Patriots">New England Patriots</option>
-<option value="New York Jets">New York Jets</option>
-<option value="Baltimore Ravens">Baltimore Ravens</option>
-<option value="Cincinnati Bengals">Cincinnati Bengals</option>
-<option value="Cleveland Browns">Cleveland Browns</option>
-<option value="Pittsburgh Steelers">Pittsburgh Steelers</option>
-<option value="Houston Texans">Houston Texans</option>
-<option value="Indianapolis Colts">Indianapolis Colts</option>
-<option value="Jacksonville Jaguars">Jacksonville Jaguars</option>
-<option value="Tennessee Titans">Tennessee Titans</option>
-<option value="Denver Broncos">Denver Broncos</option>
-<option value="Kansas City Chiefs">Kansas City Chiefs</option>
-<option value="Oakland Raiders">Oakland Raiders</option>
-<option value="San Diego Chargers">San Diego Chargers</option>
-<option value="Dallas Cowboys">Dallas Cowboys</option>
-<option value="New York Giants">New York Giants</option>
-<option value="Philadelphia Eagles">Philadelphia Eagles</option>
-<option value="Washington Redskins">Washington Redskins</option>
-<option value="Chicago Bears">Chicago Bears</option>
-<option value="Detroit Lions">Detroit Lions</option>
-<option value="Green Bay Packers">Green Bay Packers</option>
-<option value="Minnesota Vikings">Minnesota Vikings</option>
-<option value="Atlanta Falcons">Atlanta Falcons</option>
-<option value="Carolina Panthers">Carolina Panthers</option>
-<option value="New Orleans Saints">New Orleans Saints</option>
-<option value="Tampa Bay Buccaneers">Tampa Bay Buccaneers</option>
-<option value="Arizona Cardinals">Arizona Cardinals</option>
-<option value="Los Angeles Rams">Los Angeles Rams</option>
-<option value="San Francisco 49ers">San Francisco 49ers</option>
-<option value="Seattle Seahawks">Seattle Seahawks</option>
-</select></p>
+  <div style="float: left; height: 50px;">
+    Gender:
+  </div>
 
-<p>Gender: <input type="radio" name="gender" value="Female"/> Female
-<input type="radio" name="gender" value="Male"/>Male</p>
+  <div>
+    <div>
+      <input id="gender_female" type="radio" name="gender" value="Female"/>
+      <label for="gender_female">Female</label>
+    </div>
 
-<input type="submit" name="submit" value="Submit"/>
+    <div>
+      <input id="gender_male" type="radio" name="gender" value="Male"/>
+      <label for="gender_male">Male</label>
+    </div>
+  </div>
 
+  <div style="clear: both;">
+    <input type="submit" name="submit" value="Submit"/>
+  </div>
 
-</BODY>
-</HTML>
+</form>
+
+<?php
+require_once "footer.php";
--- process_form.php
+++ process_form.php
@@ -1,10 +1,15 @@
 <?php
+require_once "header.php";
+require_once "teams.php";
 
 $name=$_POST['name'];
 $team=$_POST['team'];
 $gender=$_POST['gender'];
 
-print "<p>Name: $name</br>Team: $team</br>Gender: $gender</p>";
-
-
 ?>
+<p>Name: <?= htmlentities($name); ?></p>
+<p>Team: <?= $teams[$team]; ?></p>
+<p>Gender: <?= htmlentities($gender); ?></p>
+<?php
+
+require_once "footer.php";
